c1034ab449b6712bf02026f81fbdb64927f5c603
Maxwell Heath <maxheathbusiness@gmail.com>
2023-11-29 16:32:15 -0700

    changed homerRoutes to homeRoutes and added code with notes explaining what it does

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,20 @@
+// Import the Sequelize library
 const Sequelize = require('sequelize');
+
+// Load environment variables from the .env file
 require('dotenv').config();
 
-let sequelize;
+// Create a Sequelize instance and configure it with database connection details
+const sequelize = new Sequelize(
+  process.env.DB_NAME,        // Database name
+  process.env.DB_USER,        // Database username
+  process.env.DB_PASSWORD,    // Database password
+  {
+    host: process.env.DB_HOST,  // Database host (e.g., 'localhost')
+    dialect: 'mysql',           // Specify the database dialect (MySQL in this case)
+    port: process.env.DB_PORT || 3306, // Database port (default is 3306 if not provided)
+  }
+);
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
-
+// Export the configured Sequelize instance for use in other parts of the application
 module.exports = sequelize;
--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,15 @@
+// Import the Express Router
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
+// Import other route handlers
+const apiRoutes = require('./api'); // API-related routes
+const homeRoutes = require('./homeRoutes'); // Home/dashboard-related routes
+const userRoutes = require('./userRoutes'); // User-related routes
 
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+// Define the route structure for your application
+router.use('/', homeRoutes); // Routes for the home/dashboard (root) URL
+router.use('/users', userRoutes); // Routes related to user management (e.g., registration, login, logout)
+router.use('/api', apiRoutes); // API routes for handling data requests
 
+// Export the configured router to be used in your Express application
 module.exports = router;
