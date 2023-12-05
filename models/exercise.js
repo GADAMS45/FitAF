7fb2dff017eb1744f1eaeeace4b3c75067c70dec
Maxwell Heath <maxheathbusiness@gmail.com>
2023-12-04 19:44:38 -0700

    fixed a lot of routing issues we had and linked the session key to the dotenv file

--- models/exercise.js
+++ models/exercise.js
@@ -2,19 +2,6 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Exercise extends Model {}
-
-router.get('/exercises/:exerciseType', async (req, res) => {
-    try {
-      const exerciseType = req.params.exerciseType;
-      const exerciseData = await Exercise.findAll({
-        where: { exerciseType: exerciseType }
-      });
-  
-      res.json(exerciseData);
-    } catch (error) {
-      res.status(500).send('Server Error');
-    }
-  });
   
 Exercise.init(
   {
