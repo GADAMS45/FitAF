f55aa3593bd939bc3e0f8ef5b198c5cbb1b9e46e
Trevor Christensen <trevorchristensen3405@gmail.com>
2023-12-04 18:55:28 -0700

    fixed duplicate files and router error

--- models/exercise.js
+++ models/exercise.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const router = require('express').Router();
 
 class Exercise extends Model {}
 
